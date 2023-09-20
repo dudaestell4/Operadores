9975eb368ac08cf15eb1501463c9e3ae5209e433
dudaestell4 <142898452+dudaestell4@users.noreply.github.com>
2023-09-20 07:49:32 -0300

    Add files via upload

---
+++ Operadores/operadores.js
@@ -0,0 +1,35 @@
+function somavalores(){
+
+    var s1 = document.getElementById('valor1').value;
+    var s2 = document.getElementById('valor2').value;
+    var result = parseInt(s1) + parseInt(s2);
+    
+    alert(result);
+}
+
+function subtracaoValores(){
+
+    var s1 = document.getElementById('valor1').value;
+    var s2 = document.getElementById('valor2').value;
+    var result = parseInt(s1) - parseInt(s2);
+    
+    alert(result);
+}
+
+function multiplicacaoValores(){
+
+    var s1 = document.getElementById('valor1').value;
+    var s2 = document.getElementById('valor2').value;
+    var result = parseInt(s1) * parseInt(s2);
+    
+    alert(result);
+}
+
+function divisaoValores(){
+
+    var s1 = document.getElementById('valor1').value;
+    var s2 = document.getElementById('valor2').value;
+    var result = parseInt(s1) / parseInt(s2);
+    
+    alert(result);
+}
